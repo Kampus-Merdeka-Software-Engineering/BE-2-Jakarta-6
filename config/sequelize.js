609d7ebcadd5d6ab7f811c4fc84b1a75cf7b9b1f
jeremyaryawanp <jeremyaryawanp@gmail.com>
2023-12-09 17:35:59 +0700

    Add .env

--- config/sequelize.js
+++ config/sequelize.js
@@ -1,5 +1,5 @@
 import { Sequelize } from 'sequelize';
 
-const sequelize = new Sequelize("mysql://root:Jaksa123@127.0.0.1:3306/group6-jakarta-db?allowPublicKeyRetrieval=true&useSSL=false");
+const sequelize = new Sequelize(process.env.DATABASE_URL);
 
 export default sequelize;
--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+require('dotenv').config()
+console.log(process.env)
+
 import express from 'express';
 import bodyParser from 'body-parser';
 import cors from 'cors';
